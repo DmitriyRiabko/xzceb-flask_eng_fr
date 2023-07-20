fce331972a96c938e365a327fdda9df5436fc0b6
stenloud <riabko97@gmail.com>
2023-07-20 17:24:43 -0400

    modifed unit test

--- final_project/machinetranslation/tests.py
+++ final_project/machinetranslation/tests.py
@@ -1,15 +1,22 @@
 import unittest
 
-import translator
+import translator 
 
 class TestStringMethods(unittest.TestCase):
 
     def test_french_to_english(self):
         self.assertEqual(translator.french_to_english("Bonjour"),"Hello")
 
+    def test_french_to_not_english(self):
+        self.assertNotEqual(translator.french_to_english("Bonjour"),"Bonjour")
 
-    def test_englishToFrench(self):
+
+    def test_english_to_french(self):
         self.assertEqual(translator.english_to_french("Hello"),"Bonjour")
+    
+
+    def test_english_to_not_french(self):
+        self.assertNotEqual(translator.english_to_french("Hello"),"Hello")
     
 
     
